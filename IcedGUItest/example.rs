5cef70ae9ede406f7d80b6a85f6ecc45fc1e3b53
CoolGermanGuy <EmailCGG@protonmail.com>
2025-01-20 19:13:03 +0100

    Create example.rs

---
+++ IcedGUItest/example.rs
@@ -0,0 +1,27 @@
+// example
+pub fn main() -> iced::Result {
+    iced::run("A cool counter", update, view)
+}
+
+fn update(counter: &mut Counter, message: Message) {
+    match message {
+        Message::Increment => counter.value += 1,
+    }
+}
+
+use iced::widget::{button, text};
+use iced::Element;
+
+fn view(counter: &Counter) -> Element<Message> {
+    button(text(counter.value)).on_press(Message::Increment).into()
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Increment,
+}
+
+#[derive(Default)]
+struct Counter {
+    value: u64,
+}
